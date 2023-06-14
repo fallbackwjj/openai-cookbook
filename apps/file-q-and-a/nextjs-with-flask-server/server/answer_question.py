58f36aacf5c50b03d7d3e16f4a725a61bf1745a8
马骉 <mabiao@100tal.com>
2023-06-14 14:33:21 +0800

    20230614 update in langchain version

--- apps/file-q-and-a/nextjs-with-flask-server/server/answer_question.py
+++ apps/file-q-and-a/nextjs-with-flask-server/server/answer_question.py
@@ -1,9 +1,6 @@
-import json
-from utils import get_embedding
-from flask import jsonify
 from config import *
-from flask import current_app
-
+from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts.chat import (
@@ -12,12 +9,8 @@ from langchain.prompts.chat import (
     SystemMessagePromptTemplate,
 )
 
-from config import *
-
-TOP_K = 4
-
-
 def get_answer_from_files(question, session_id, pinecone_index):
+    # parse question
     logging.warning(f"Getting answer for question: {question}")
     userMsg = list(filter(lambda item: item['role'] == 'user', question['messages']))
     systemMsg = list(filter(lambda item: item['role'] == 'system', question['messages']))
@@ -25,45 +18,25 @@ def get_answer_from_files(question, session_id, pinecone_index):
     last_system_msg = systemMsg[-1] if systemMsg else None
     logging.warning(f"system: {last_system_msg}")
     logging.warning(f"user {last_user_msg}")
-    search_query_embedding = get_embedding(last_user_msg['content'], EMBEDDINGS_MODEL)
+    
     try:
-        query_response = pinecone_index.query(
-            namespace=session_id,
-            top_k=TOP_K,
-            include_values=False,
-            include_metadata=True,
-            vector=search_query_embedding,
-        )
-        # logging.warning(
-        #     f"[get_answer_from_files] received query response from Pinecone: {query_response}")
+        # get_relevant_documents
+        docsearch = Pinecone.from_existing_index(index_name=PINECONE_INDEX, 
+            embedding=OpenAIEmbeddings(), text_key="text", namespace=session_id)
+        simDocs = docsearch.similarity_search(last_user_msg['content'], k = 50)
+        logging.warning(simDocs)
         files_string = ""
-        file_text_dict = current_app.config["file_text_dict"]
-        listChunkList = {}
-        for i in range(len(query_response.matches)):
-            result = query_response.matches[i]
-            file_chunk_id = result.id
-            score = result.score 
-            filename = result.metadata["filename"]
-            filechunkid = result.metadata["file_chunk_index"]
-            file_text = file_text_dict.get(file_chunk_id)
-            # file_string = f"###\n\"{filename}\"\n{file_text}\n"
-            file_string = f"{file_text}"
-            if score < COSINE_SIM_THRESHOLD and i > 0:
-                logging.warning(
-                    f"[get_answer_from_files] score {score} is below threshold {COSINE_SIM_THRESHOLD} and i is {i}, breaking")
-                break
-            if len(file_text) != 0:
-                listChunkList[filechunkid] = file_text
-            files_string += file_string
+        for i, doc in enumerate(simDocs):
+            files_string += f"\n{doc.page_content}\n"
         logging.warning(f"files_string {files_string}")
-
+        files_string = files_string[0:2500]
+        # ask chatgpt
         prompt=ChatPromptTemplate.from_messages(
             [
                 SystemMessagePromptTemplate.from_template(last_system_msg["content"]), 
                 HumanMessagePromptTemplate.from_template(last_user_msg["content"])
             ]
         )
-        # echo de
         promptMsg=prompt.format_prompt(files_string=files_string, question=last_user_msg["content"]).to_string()
         chain = LLMChain(
             llm=ChatOpenAI(model=GENERATIVE_MODEL, temperature=0, max_tokens=500),
@@ -71,35 +44,6 @@ def get_answer_from_files(question, session_id, pinecone_index):
             verbose=True,
         )
         answer = chain.run(files_string=files_string, question=last_user_msg["content"])
-
-        # Note: this is not the proper way to use the ChatGPT conversational format, but it works for now
-        # messages = [
-        #     {
-        #         "role": "system",
-        #         "content": f"Given a question, try to answer it using the content of the file extracts below, and if you cannot answer, or find " \
-        #         f"a relevant file, just output \"I couldn't find the answer to that question in your files.\".\n\n" \
-        #         f"If the answer is not contained in the files or if there are no file extracts, respond with \"I couldn't find the answer " \
-        #         f"to that question in your files.\" If the question is not actually a question, respond with \"That's not a valid question.\"\n\n" \
-        #         f"In the cases where you can find the answer, first give the answer. Then explain how you found the answer from the source or sources, " \
-        #         f"and use the exact filenames of the source files you mention. Do not make up the names of any other files other than those mentioned "\
-        #         f"in the files context. Give the answer in markdown format." \
-        #         f"Use the following format:\n\nQuestion: <question>\n\nFiles:\n<###\n\"filename 1\"\nfile text>\n<###\n\"filename 2\"\nfile text>...\n\n"\
-        #         f"Answer: <answer or \"I couldn't find the answer to that question in your files\" or \"That's not a valid question.\">\n\n" \
-        #         f"Question: {question}\n\n" \
-        #         f"Files:\n{files_string}\n" \
-        #         f"Answer:"
-        #     },
-        # ]
-
-        # response = openai.ChatCompletion.create(
-        #     messages=messages,
-        #     model=GENERATIVE_MODEL,
-        #     max_tokens=1000,
-        #     temperature=0,
-        # )
-        # choices = response["choices"]  # type: ignore
-        # answer = choices[0].message.content.strip()
-
         resDict = {
             "Answer" : answer,
             "Prompt" : promptMsg,
@@ -111,3 +55,4 @@ def get_answer_from_files(question, session_id, pinecone_index):
     except Exception as e:
         logging.warning(f"[get_answer_from_files] error: {e}")
         return str(e)
+
--- apps/file-q-and-a/nextjs-with-flask-server/server/handle_file.py
+++ apps/file-q-and-a/nextjs-with-flask-server/server/handle_file.py
@@ -1,27 +1,26 @@
 import logging
 import sys
-import docx2txt
-import textwrap
+import os
+import hashlib
 
-from PyPDF2 import PdfReader
 from numpy import array, average
-from flask import current_app
 from config import *
 
 from langchain import OpenAI, PromptTemplate
 from langchain.chat_models import ChatOpenAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
 from langchain.chains.summarize import load_summarize_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
+from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredWordDocumentLoader
 
 
 from utils import get_embeddings, get_pinecone_id_for_file_chunk
 
 # Set up logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
         logging.FileHandler("debug.log"),
@@ -34,202 +33,84 @@ def handle_file(file, session_id, pinecone_index, tokenizer):
     """Handle a file by extracting its text, creating embeddings, and upserting them to Pinecone."""
     filename = file.filename
     logging.info("[handle_file] Handling file: {}".format(filename))
+    # # Get the file text dict from the current app config
+    # file_text_dict = current_app.config["file_text_dict"]
 
-    # Get the file text dict from the current app config
-    file_text_dict = current_app.config["file_text_dict"]
+    # save file string to fileDic
+    fileDic = f"/home/ec2-user/tmpfile"
+    if not os.path.exists(fileDic):
+        os.makedirs(fileDic)
+    filePath = os.path.join(fileDic, filename)
+    file.save(filePath)
 
-    # Extract text from the file
+    # loader
     try:
-        extracted_text, pdf_list = extract_text_from_file(file)
+        if file.mimetype == "application/pdf":
+            loader = UnstructuredPDFLoader(filePath, mode="elements")
+        elif file.mimetype == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            loader = UnstructuredWordDocumentLoader(filePath, mode="elements")
+        else:
+            # Unsupported file type
+            raise ValueError("Unsupported file type: {}".format(file.mimetype))
     except ValueError as e:
-        logging.error(
-            "[handle_file] Error extracting text from file: {}".format(e))
+        logging.error("[handle_file] Error extracting text from file: {}".format(e))
         raise e
+    docs = loader.load()
+   
+    # splitters
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=80,
+        length_function=len,
+        add_start_index=True,
+    )
+    docsChunks = text_splitter.split_documents(docs)
+    logging.warning(len(docsChunks))
 
-    # Save extracted text to file text dict
-    file_text_dict[filename] = extracted_text
-
-    # Handle the extracted text as a string
-    return handle_file_string(filename, session_id, extracted_text, pinecone_index, tokenizer, file_text_dict, pdf_list)
-
-# Extract text from a file based on its mimetype
-def extract_text_from_file(file):
-    """Return the text content of a file."""
-    pdf_list = []
-    if file.mimetype == "application/pdf":
-        # Extract text from pdf using PyPDF2
-        reader = PdfReader(file)
-        extracted_text = ''
-        for i, page in enumerate(reader.pages):
-            text = page.extract_text()
-            if text:
-                extracted_text += text
-                pdf_list.append(text)
-    elif file.mimetype == "text/plain":
-        # Read text from plain text file
-        extracted_text = file.read().decode("utf-8")
-        file.close()
-    elif file.mimetype == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-        # Extract text from docx using docx2txt
-        extracted_text = docx2txt.process(file)
-    else:
-        # Unsupported file type
-        raise ValueError("Unsupported file type: {}".format(file.mimetype))
-
-    return (extracted_text, pdf_list)
-
-# Handle a file string by creating embeddings and upserting them to Pinecone
-def handle_file_string(filename, session_id, file_body_string, pinecone_index, tokenizer, file_text_dict, pdf_list):
-    """Handle a file string by creating embeddings and upserting them to Pinecone."""
-    logging.warning("[handle_file_string] Starting...")
-
-    # Clean up the file string by replacing newlines and double spaces
-    clean_file_body_string = file_body_string.replace(
-        "\n", "; ").replace("  ", " ")
-    # Add the filename to the text to embed
-    text_to_embed = "Filename is: {}; {}".format(
-        filename, clean_file_body_string)
-
-    # Create embeddings for the text
-    try:
-        text_embeddings, average_embedding, output_summary = create_embeddings_for_text(
-            text_to_embed, tokenizer, pdf_list)
-        logging.info(
-            "[handle_file_string] Created embedding for {}".format(filename))
-    except Exception as e:
-        logging.error(
-            "[handle_file_string] Error creating embedding: {}".format(e))
-        raise e
-
-    # Get the vectors array of triples: file_chunk_id, embedding, metadata for each embedding
-    # Metadata is a dict with keys: filename, file_chunk_index
-    vectors = []
+    # vectorstone
     i = 1
-    for i, (text_chunk, embedding) in enumerate(text_embeddings):
-        id = get_pinecone_id_for_file_chunk(session_id, filename, i)
-        file_text_dict[id] = text_chunk
-        vectors.append(
-            (id, embedding, {"filename": filename, "file_chunk_index": i}))
+    vectorIds = []
+    for i, doc in enumerate(docsChunks):
+        vectorIds.append(get_pinecone_id_for_file_chunk(session_id, filename, i))
+    docsearch= Pinecone.from_documents(documents=docs, text_key="text",ids=vectorIds, embedding=OpenAIEmbeddings(),index_name=PINECONE_INDEX, namespace=session_id)
+    # simDocs = docsearch.similarity_search("summary")
+   
+    # summary (todo: use llamaIndex summary)
+    output_summary = "True"
+    '''
+    prompt_template = """Generate a summary of the following document, arranging the top 5 points in sequential order starting from 1, with the highest semantic relevance, while maintaining the language consistency of the document: 
+    
+    {text}
+    
+    """
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
+    chain = load_summarize_chain(ChatOpenAI(temperature=0, model=GENERATIVE_MODEL, max_tokens=500), 
+        chain_type="map_reduce", map_prompt=PROMPT, combine_prompt=PROMPT) 
+    # chain = load_summarize_chain(OpenAI(temperature=0, model="curie", max_tokens=300), 
+    #     chain_type="map_reduce", map_prompt=PROMPT, combine_prompt=PROMPT, ver)
+    output_summary = chain({"input_documents": docs}, return_only_outputs=True)  
+    logging.warning(output_summary)      
+    
+    wrapped_text = textwrap.fill(output_summary, 
+        width=100,
+        break_long_words=False,
+        replace_whitespace=False)
 
-        logging.info(
-            "[handle_file_string] Text chunk {}: {}".format(i, text_chunk))
-
-    # Split the vectors array into smaller batches of max length 2000
-    batch_size = MAX_PINECONE_VECTORS_TO_UPSERT_PATCH_SIZE
-    batches = [vectors[i:i+batch_size] for i in range(0, len(vectors), batch_size)]
-    # Upsert each batch to Pinecone
-    for batch in batches:
-        try:
-            pinecone_index.upsert(
-                vectors=batch, namespace=session_id)
-
-            logging.info(
-                "[handle_file_string] Upserted batch of embeddings for {}".format(filename))
-        except Exception as e:
-            logging.error(
-                "[handle_file_string] Error upserting batch of embeddings to Pinecone: {}".format(e))
-            raise e
+    refine_template = ("Your job is to produce a final summary\n"
+        "We have provided an existing summary up to a certain point: {existing_answer}\n"
+        "We have the opportunity to refine the existing summary"
+        "(only if needed) with some more context below.\n"
+        "------------\n"
+        "{text}\n"
+        "------------\n"
+        "Given the new context, refine the original summary"
+        "If the context isn't useful, return the original summary."
+        "Write a concise bullet point summary."
+    )
+    question_prompt = PromptTemplate(input_variables=["text"], template=prompt_template)
+    refine_prompt = PromptTemplate(input_variables=["existing_answer", "text"], template=refine_template)
+    chain = load_summarize_chain(OpenAI(temperature=0, model=GENERATIVE_MODEL, max_tokens=1000), chain_type="refine", 
+        return_intermediate_steps=True, question_prompt=question_prompt, refine_prompt=refine_prompt)
+    output_summary = chain({"input_documents": docs}, return_only_outputs=True)
+    '''
     return output_summary
-
-# Compute the column-wise average of a list of lists
-def get_col_average_from_list_of_lists(list_of_lists):
-    """Return the average of each column in a list of lists."""
-    if len(list_of_lists) == 1:
-        return list_of_lists[0]
-    else:
-        list_of_lists_array = array(list_of_lists)
-        average_embedding = average(list_of_lists_array, axis=0)
-        return average_embedding.tolist()
-
-# Create embeddings for a text using a tokenizer and an OpenAI engine
-def create_embeddings_for_text(text, tokenizer, pdf_list):
-    """Return a list of tuples (text_chunk, embedding) and an average embedding for a text."""
-    # pdf按页切分
-    if len(pdf_list) == 0:
-        token_chunks = list(chunks(text, TEXT_EMBEDDING_CHUNK_SIZE, tokenizer))
-        text_chunks = [tokenizer.decode(chunk) for chunk in token_chunks]
-        # Split text_chunks into shorter arrays of max length 10
-        text_chunks_arrays = [text_chunks[i:i+MAX_TEXTS_TO_EMBED_BATCH_SIZE] for i in range(0, len(text_chunks), MAX_TEXTS_TO_EMBED_BATCH_SIZE)]
-    else:
-        text_chunks = pdf_list
-        text_chunks_arrays = pdf_list
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=800,
-            chunk_overlap=80,
-            length_function=len
-        )
-        text_chunks = text_splitter.split_text(text)
-        text_chunks_arrays = text_chunks 
-
-        docs = [Document(page_content=t) for t in text_chunks]
-        logging.warning(len(docs))
-        # docsearch = Pinecone.from_documents(docs, OpenAIEmbeddings(), index_name=PINECONE_INDEX)
-        # res = docsearch.similarity_search("summary")
-        # logging.warning(f"pinecone:{res}")
-
-        # todo 多账号并发
-        # prompt_template = """Generate a summary of the following document, arranging the top 5 points in sequential order starting from 1, with the highest semantic relevance, while maintaining the language consistency of the document: 
-       
-        # {text}
-        
-        # """
-        # PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
-        # chain = load_summarize_chain(ChatOpenAI(temperature=0, model=GENERATIVE_MODEL, max_tokens=500), 
-        #     chain_type="map_reduce", map_prompt=PROMPT, combine_prompt=PROMPT) 
-        # # chain = load_summarize_chain(OpenAI(temperature=0, model="curie", max_tokens=300), 
-        # #     chain_type="map_reduce", map_prompt=PROMPT, combine_prompt=PROMPT, ver)
-        # output_summary = chain({"input_documents": docs}, return_only_outputs=True)  
-        # logging.warning(output_summary)      
-       
-        # wrapped_text = textwrap.fill(output_summary, 
-        #     width=100,
-        #     break_long_words=False,
-        #     replace_whitespace=False)
-
-        # refine_template = ("Your job is to produce a final summary\n"
-        #     "We have provided an existing summary up to a certain point: {existing_answer}\n"
-        #     "We have the opportunity to refine the existing summary"
-        #     "(only if needed) with some more context below.\n"
-        #     "------------\n"
-        #     "{text}\n"
-        #     "------------\n"
-        #     "Given the new context, refine the original summary"
-        #     "If the context isn't useful, return the original summary."
-        #     "Write a concise bullet point summary."
-        # )
-        # question_prompt = PromptTemplate(input_variables=["text"], template=prompt_template)
-        # refine_prompt = PromptTemplate(input_variables=["existing_answer", "text"], template=refine_template)
-        # chain = load_summarize_chain(OpenAI(temperature=0, model=GENERATIVE_MODEL, max_tokens=1000), chain_type="refine", 
-        #     return_intermediate_steps=True, question_prompt=question_prompt, refine_prompt=refine_prompt)
-        # output_summary = chain({"input_documents": docs}, return_only_outputs=True)
-        
-
-    # Call get_embeddings for each shorter array and combine the results
-    embeddings = []
-    for text_chunks_array in text_chunks_arrays:
-        embeddings_response = get_embeddings(text_chunks_array, EMBEDDINGS_MODEL)
-        embeddings.extend([embedding["embedding"] for embedding in embeddings_response])
-
-    text_embeddings = list(zip(text_chunks, embeddings))
-    average_embedding = get_col_average_from_list_of_lists(embeddings)
-    return (text_embeddings, average_embedding, " ")
-
-# Split a text into smaller chunks of size n, preferably ending at the end of a sentence
-def chunks(text, n, tokenizer):
-    tokens = tokenizer.encode(text)
-    """Yield successive n-sized chunks from text."""
-    i = 0
-    while i < len(tokens):
-        # Find the nearest end of sentence within a range of 0.5 * n and 1.5 * n tokens
-        j = min(i + int(1.5 * n), len(tokens))
-        while j > i + int(0.5 * n):
-            # Decode the tokens and check for full stop or newline
-            chunk = tokenizer.decode(tokens[i:j])
-            if chunk.endswith(".") or chunk.endswith("\n"):
-                break
-            j -= 1
-        # If no end of sentence found, use n tokens as the chunk size
-        if j == i + int(0.5 * n):
-            j = min(i + n, len(tokens))
-        yield tokens[i:j]
-        i = j
